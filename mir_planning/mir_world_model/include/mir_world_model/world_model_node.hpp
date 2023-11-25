84137c7df4f1fde92640162af2fa8b1315aeb491
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2023-11-25 12:51:13 +0100

    linked world model as a library to node

--- mir_planning/mir_world_model/include/mir_world_model/world_model_node.hpp
+++ mir_planning/mir_world_model/include/mir_world_model/world_model_node.hpp
@@ -1,3 +1,7 @@
 /**
  * @brief world_model ros node
 */
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "mir_world_model/world_model.hpp"
--- mir_planning/mir_world_model/src/world_model_node.cpp
+++ mir_planning/mir_world_model/src/world_model_node.cpp
@@ -1,1 +1,9 @@
 #include "mir_world_model/world_model_node.hpp"
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  // rclcpp::spin(std::make_shared<WorldModelNode>());
+  rclcpp::shutdown();
+  return 0;
+}
