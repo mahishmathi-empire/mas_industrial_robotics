36d9d034bc647e72f470750005c6bc7cc3675f5f
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2023-11-30 10:50:06 +0100

    added high level BT package

---
+++ mir_planning/mir_actions/mir_big_tree/include/mir_big_tree/big_tree.hpp
@@ -0,0 +1,7 @@
+#ifndef MIR_BIG_TREE_HPP
+#define MIR_BIG_TREE_HPP
+
+#include "mir_perceive_location/perceive_location.hpp"
+#include "mir_pick_object/pick_object.hpp"
+
+#endif  // MIR_BIG_TREE_HPP
---
+++ mir_planning/mir_actions/mir_big_tree/src/big_tree.cpp
@@ -0,0 +1,57 @@
+#include "mir_big_tree/big_tree.hpp"
+
+int
+main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+
+  // create a node
+  auto node = std::make_shared<rclcpp::Node>("BigTree");
+
+  BT::BehaviorTreeFactory factory;
+
+  // register actions
+  factory.registerNodeType<PerceiveLocationAction>("PerceiveLocationAction", node);
+  factory.registerNodeType<PickObjectAction>("PickObjectAction", node);
+
+  // get xml file
+  std::string pkg_path =
+    ament_index_cpp::get_package_share_directory("mir_big_tree");
+
+  std::string bt_path = pkg_path + "/bt/";
+
+  // load all xml files in this directory
+  for (const auto& entry : std::filesystem::directory_iterator(bt_path)) {
+    if (entry.path().extension() == ".xml") {
+      factory.registerBehaviorTreeFromFile(entry.path().string());
+    }
+  }
+
+  BT::Tree tree = factory.createTree("MainTree");
+
+  // tick the root of the tree
+  // tree.tickWhileRunning();
+
+  std::cout << "--- ticking\n";
+  auto status = tree.tickOnce();
+  std::cout << "--- status: " << toStr(status) << "\n\n";
+
+  while (rclcpp::ok() && status == BT::NodeStatus::RUNNING) {
+    // Sleep to avoid busy loops.
+    // do NOT use other sleep functions!
+    // Small sleep time is OK, here we use a large one only to
+    // have less messages on the console.
+    tree.sleep(std::chrono::milliseconds(1));
+
+    // spin
+    rclcpp::spin_some(node);
+
+    std::cout << "--- ticking\n";
+    status = tree.tickOnce();
+    std::cout << "--- status: " << toStr(status) << "\n\n";
+  }
+
+  rclcpp::shutdown();
+
+  return 0;
+}
