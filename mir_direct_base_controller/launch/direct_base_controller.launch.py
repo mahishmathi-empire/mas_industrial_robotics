9da4f653097df3d69a6bea594e846d3b3ec86b59
Arnold-Dsouza <arnold.dsouza@smail.inf.h-brs.de>
2023-12-01 15:20:43 +0100

    Added launch file and added install directory in Cmakelist

---
+++ mir_direct_base_controller/launch/direct_base_controller.launch.py
@@ -0,0 +1,23 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, Command, FindExecutable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterValue
+import os
+
+def generate_launch_description():
+
+    # add the gripper controller node
+    DirectBaseController = Node(
+        package='mir_direct_base_controller',
+        executable='direct_base_controller',
+        name='direct_base_controller',
+        output='both',
+        remappings=[('cmd_vel', '/cmd_vel'), ('target_pose', '/target_pose')]
+    )
+
+    return LaunchDescription([
+        DirectBaseController,
+    ])
